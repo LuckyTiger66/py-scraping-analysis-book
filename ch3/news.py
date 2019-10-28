bd40bb1f5e83203a1014182ef5c17f580071406d
LuckyTiger66 <start1002001@gmail.com>
2019-10-28 14:25:39 +0800

    :wq

--- ch3/news.py
+++ ch3/news.py
@@ -4,17 +4,16 @@ from bs4 import BeautifulSoup
 
 def main():
     print('蘋果今日焦點')
-    dom = requests.get('http://www.appledaily.com.tw/appledaily/hotdaily/headline').text
+    dom = requests.get('https://tw.appledaily.com/hot/daily').text
     soup = BeautifulSoup(dom, 'html5lib')
     for ele in soup.find('ul', 'all').find_all('li'):
         print(
             ele.find('div', 'aht_title_num').text,
-            ele.find('div', 'aht_title').text,
-            ele.find('div', 'aht_pv_num').text
+            ele.find('div', 'aht_title').text
         )
     print('-----------')
     print('自由今日焦點')
-    dom = requests.get('http://news.ltn.com.tw/list/newspaper').text
+    dom = requests.get('https://news.ltn.com.tw/list/breakingnews').text
     soup = BeautifulSoup(dom, 'html5lib')
     for ele in soup.find('ul', 'list').find_all('li'):
         if ele.find('a', 'tit'):
